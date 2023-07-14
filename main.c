947de77f895567b7a4d28f5b85a7562fe5ac0cae
programmer838 <71629437+programmer838@users.noreply.github.com>
2023-07-14 16:50:43 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,44 +1,24 @@
 #include "uefi_structures.h"
 #include "uefi_lib.h"
 
+
 EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     EFI_STATUS Status;
-
-    clearScreen(SystemTable); // clear screen
-
     EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
-    EFI_GRAPHICS_OUTPUT_BLT_PIXEL WhitePixel = {255, 255, 255, 0};
     SystemTable->BootServices->LocateProtocol(&EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID, NULL, (VOID **)&Gop);
 
-    UINTN Width = Gop->Mode->Info->HorizontalResolution;
-    UINTN Height = Gop->Mode->Info->VerticalResolution;
-
-    UINTN TextCols;
-    UINTN TextRows;
+    clearScreen(SystemTable); // Clear screen
+    borderOutline(Gop);       // Create white border
 
     setTextPosition(SystemTable, 4, 2);
-    Print(SystemTable, L"Welcome to the SIMPLE Bootloader");
+    Print(SystemTable, L"Welcome to the SIMPLE Bootloader"); // Print Hello message
 
-    borderOutline(Gop, &WhitePixel, Width, Height);
     setTextColour(SystemTable, EFI_YELLOW);
+    printOptions(SystemTable, Gop);                      // Print Bootloader options
 
-    getNumRowAndCol(SystemTable, &TextCols, &TextRows);
-    setTextPosition(SystemTable, 0, TextRows - 5); // adjust the row number for each option
-    Print(SystemTable, L"[Press 'b' to boot]\n");
-
-    setTextPosition(SystemTable, 0, TextRows - 4); // move cursor down
-    Print(SystemTable, L"[Press 'i' to print system specs]\n");
-
-    setTextPosition(SystemTable, 0, TextRows - 3); // move cursor down
-    Print(SystemTable, L"[Press 'r' to reboot]\n");
-
-    setTextPosition(SystemTable, 0, TextRows - 2); // move cursor down
-    Print(SystemTable, L"[Press 's' to shutdown]\n");
-
-    setTextColour(SystemTable, EFI_LIGHTCYAN);
     SystemTable->ConIn->Reset(SystemTable->ConIn, 1); // clear keyboard buffer
 
     EFI_INPUT_KEY Key;
@@ -49,13 +29,13 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
         if (Key.UnicodeChar == L'r' || Key.UnicodeChar == L'R')
         {
-            setTextPosition(SystemTable, 5, 2);
+            setTextPosition(SystemTable, 2, 5);
             Print(SystemTable, L"Rebooting...\n");
             SystemTable->RuntimeServices->ResetSystem(EfiResetCold, EFI_SUCCESS, 0, NULL);
         }
         else if (Key.UnicodeChar == L'b' || Key.UnicodeChar == L'B')
         {
-            setTextPosition(SystemTable, 5, 2);
+            setTextPosition(SystemTable, 2, 5);
             Print(SystemTable, L"Booting...\n");
         }
     } while (Status == EFI_NOT_READY || !(Key.UnicodeChar == L'r' || Key.UnicodeChar == L'R' || Key.UnicodeChar == L'b' || Key.UnicodeChar == L'B'));
--- uefi_lib.h
+++ uefi_lib.h
@@ -31,31 +31,57 @@ void setPixel(EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop, UINT32 x, UINT32 y, EFI_GRAPHIC
      Gop->Blt(Gop, Colour, EfiBltVideoFill, 0, 0, x, y, 1, 1, 0);
 }
 
-void borderOutline(EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop, EFI_GRAPHICS_OUTPUT_BLT_PIXEL *Colour, UINTN Width, UINTN Height)
+void borderOutline(EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop)
 {
      /*
         This function creates a two pixel wide border on the screen
         with a 10 pixel margin from the edge
      */
 
+     EFI_GRAPHICS_OUTPUT_BLT_PIXEL WhitePixel = {255, 255, 255, 0};
+
+     UINTN Width = Gop->Mode->Info->HorizontalResolution;
+     UINTN Height = Gop->Mode->Info->VerticalResolution;
+
      for (UINTN X = 10; X < Width - 10; X++)
      {
-          setPixel(Gop, X, 10, Colour);              // Top outer pixel
-          setPixel(Gop, X, 10 + 1, Colour);          // Top inner pixel
-          setPixel(Gop, X, Height - 1 - 10, Colour); // Bottom outer pixel
-          setPixel(Gop, X, Height - 2 - 10, Colour); // Bottom inner pixel
+          setPixel(Gop, X, 10, &WhitePixel);              // Top outer pixel
+          setPixel(Gop, X, 10 + 1, &WhitePixel);          // Top inner pixel
+          setPixel(Gop, X, Height - 1 - 10, &WhitePixel); // Bottom outer pixel
+          setPixel(Gop, X, Height - 2 - 10, &WhitePixel); // Bottom inner pixel
      }
 
      for (UINTN Y = 10; Y < Height - 10; Y++)
      {
-          setPixel(Gop, 10, Y, Colour);             // Left outer pixel
-          setPixel(Gop, 10 + 1, Y, Colour);         // Left inner pixel
-          setPixel(Gop, Width - 1 - 10, Y, Colour); // Right outer pixel
-          setPixel(Gop, Width - 2 - 10, Y, Colour); // Right inner pixel
+          setPixel(Gop, 10, Y, &WhitePixel);             // Left outer pixel
+          setPixel(Gop, 10 + 1, Y, &WhitePixel);         // Left inner pixel
+          setPixel(Gop, Width - 1 - 10, Y, &WhitePixel); // Right outer pixel
+          setPixel(Gop, Width - 2 - 10, Y, &WhitePixel); // Right inner pixel
      }
 }
 
 void getNumRowAndCol(EFI_SYSTEM_TABLE *SystemTable, UINTN *Cols, UINTN *Rows)
 {
      SystemTable->ConOut->QueryMode(SystemTable->ConOut, SystemTable->ConOut->Mode->Mode, Cols, Rows);
+}
+
+void printOptions(EFI_SYSTEM_TABLE *SystemTable, EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop)
+{
+     SystemTable->BootServices->LocateProtocol(&EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID, NULL, (VOID **)&Gop);
+
+     UINTN TextCols;
+     UINTN TextRows;
+
+     getNumRowAndCol(SystemTable, &TextCols, &TextRows);
+     setTextPosition(SystemTable, 0, TextRows - 5); // adjust the row number for each option
+     Print(SystemTable, L"[Press 'b' to boot]\n");
+
+     setTextPosition(SystemTable, 0, TextRows - 4); // move cursor down
+     Print(SystemTable, L"[Press 'i' to print system specs]\n");
+
+     setTextPosition(SystemTable, 0, TextRows - 3); // move cursor down
+     Print(SystemTable, L"[Press 'r' to reboot]\n");
+
+     setTextPosition(SystemTable, 0, TextRows - 2); // move cursor down
+     Print(SystemTable, L"[Press 's' to shutdown]\n");
 }
